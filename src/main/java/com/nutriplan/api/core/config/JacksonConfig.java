6e82e949b80f0ed34b8b43a462f0c86832cd8733
PEDRO\Pedro <pedro.j.izdo@gmail.com>
2026-03-21 16:18:05 +0100

    fix(api): standardize datetime handling in UTC

--- src/main/java/com/nutriplan/api/core/config/JacksonConfig.java
+++ src/main/java/com/nutriplan/api/core/config/JacksonConfig.java
@@ -1,18 +1,25 @@
 package com.nutriplan.api.core.config;
 
+import java.util.TimeZone;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
+
+
 
 @Configuration
 public class JacksonConfig {
 
-    @Bean
+   @Bean
     public ObjectMapper objectMapper() {
         return JsonMapper.builder()
                 .findAndAddModules()
-                .build();
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+                .build()
+                .setTimeZone(TimeZone.getTimeZone("UTC"));
     }
 }
